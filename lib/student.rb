7d98a4243a6ccef555ad2728c73547ce40ca3cc4
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-04-09 00:13:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,6 +51,20 @@ class Student
   end
   
   def self.find_by_name(name)
+    sql = <<-SQL
+    SELECT * FROM students WHERE name = ?
+    SQL
+    DB[:conn].execute(sql, name).each do |row|
+      self.new_from_db(row)
+    end
+    
+  end
+  
+  def update
+    sql = <<-SQL
+    UPDATE students SET name = ?, grade = ? WHERE id = ?
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 
 
